afe364a331ba2010e236e7214680d5921b0aec1a
Alam <Alam@niteshbatav57gmail.onmicrosoft.com>
2024-07-21 05:54:44 +0000

    Updated provider.tf

--- Environment/Prod/provider.tf
+++ Environment/Prod/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.113.0"
     }
   }
+    backend "azurerm" {
+    resource_group_name  = "ABC"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storageabcd123456789"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "state"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+}
 }
 
 provider "azurerm" {
