41bb45f213c36d61e4ada5d550ed6b739fe8c6a5
alambijali9 <alambijali9@gmail.com>
2024-09-18 18:27:46 +0530

    infra

--- Environment/Prod/provider.tf
+++ Environment/Prod/provider.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name  = "ABC"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "storageabcd123456789"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "state"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "Prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
 }
 }
 
